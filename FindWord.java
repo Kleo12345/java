ee0aa383c3bf22f1e3b2291a0a2cd7a932c0a9c1
Kleo <150968951+Kleo12345@users.noreply.github.com>
2025-04-20 19:25:21 +0200

    Update and rename Main.java to FindWord.java

--- FindWord.java
+++ FindWord.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class FindWord {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String phrase = scan.nextLine();
